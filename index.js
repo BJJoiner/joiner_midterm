b0b23971e4cd9836efc6badeea539b9633fddcc3
BJJoiner <joiner_brandon@columbusstate.edu>
2020-11-09 20:14:06 +0000

    phase 2

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-const port = process.env.port || 3000;
-app.listen(port, function(){};
+var express = require('express');
+
+var bodyParser = require ("body-parser");
+
+var fetch = require('node-fetch');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("Public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    fetch('https://xkcd.com/info.0.json',)
+    .then(res => res.json())
+    .then(data => {
+        res.render('index', {data:data});
+    });
+})
+
+app.get('/xkcd', function(req, res){ 
+    function numGenerator(){
+    let numRandom = Math.random();
+    return Math.floor(numRandom);
+};
+    let num = numGenerator();
+    fetch('https://xkcd.com/' +num + '/info.0.json',)
+    .then(res => res.json())
+    .then(data => {
+        res.render('xkcd', {data:data});
+    });
+})
+
+
+app.listen(port,function(){
+    console.log('listening on ' + port)
+});
